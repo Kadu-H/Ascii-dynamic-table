f92216b87c92eaf45165b71dd72c5ad2947d5490
Kdu <olocgame@gmail.com>
2023-06-02 14:34:54 -0300

    criando a função readString

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <cstring>
+
+int readString(){
+
+    char caracteres[255];
+    int codigoChar;
+
+    std::cout<<"Digite a sequencia de caracteres: ";
+    setbuf(stdin,0);
+
+    fgets(caracteres,255,stdin);
+
+    caracteres[strlen(caracteres)-1] = '\0';
+
+    for(int i = 0; i<=strlen(caracteres); i++){
+        codigoChar = caracteres[i];
+        std::cout<<"\n"<<caracteres[i]<<" = "<<codigoChar;
+    }
+    
+
+    return 0;
+}
 
 int main(){
-    
+    readString();
     return 0;
 }
